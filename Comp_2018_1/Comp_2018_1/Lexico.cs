dd727f6faa0f61e487b0405178234a98c4fef282
renatofelix <renatofelixmartins@gmail.com>
2018-04-09 13:26:10 -0300

    first commit

---
+++ Comp_2018_1/Comp_2018_1/Lexico.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp_2018_1
+{
+    class Lexico
+    {
+        public struct Table_simbols
+        {
+            public Token token;
+            public string lexema;
+            public string tipo;
+        }
+
+        public int MachineState(int actualState)
+        {
+            return 0;
+        }
+
+        public enum Token
+        {
+            Num,
+            Lietral,
+            Id,
+            Comentario,
+            Tab,
+            Salto,
+            Espaço,
+            EOF,
+            OPR,
+            RCB,
+            OPM,
+            AB_P,
+            FC_P,
+            ERRO
+        }
+    }
+}

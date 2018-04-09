b2afe6aa92fc81f8ea4944ea5a2022dbdf398c44
renatofelix <renatofelixmartins@gmail.com>
2018-04-09 13:49:32 -0300

    first commit

--- Comp_2018_1/Comp_2018_1/Program.cs
+++ Comp_2018_1/Comp_2018_1/Program.cs
@@ -6,15 +6,13 @@ namespace Comp_2018_1
     {
         static void Main(string[] args)
         {
-            string text = System.IO.File.ReadAllText(@"C:\Users\Renato\Documents\Visual Studio 2017\Projects\Comp_2018_1\Comp_2018_1\text_file.txt");
+            string text = System.IO.File.ReadAllText(@"C:\Users\Renato\compilador\Comp_2018_1\Comp_2018_1\text_file.txt");
             char[] text_Char = text.ToCharArray();
             //System.Console.WriteLine(text);
             for(int i = 0; i< text_Char.Length;i++)
             {
-                System.Console.Write(text_Char[i]);
+               System.Console.Write(text_Char[i]);
             }
-            
-
             Console.ReadKey(true);
         }
     }
